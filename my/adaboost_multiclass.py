c72abe79cfba85b2d616aeecb12ac9e7b3fb4816
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2015-04-29 12:22:40 +0200

    split for multiclass adaboost

--- my/adaboost_multiclass.py
+++ my/adaboost_multiclass.py
@@ -1,118 +1,70 @@
 from numpy import *
 
-def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
+def assignIntervals(dataMatrix, dimen, intervals, assignBits):
+	intsMat = mat(intervals)
 	retArray = ones((shape(dataMatrix)[0],1))
-	if threshIneq == 'lt':
-		retArray[dataMatrix[:,dimen] <= threshVal] = -1.0
-	else:
-		retArray[dataMatrix[:,dimen] > threshVal] = -1.0
+	m = shape(intsMat)[1]
+	lowerBound = intsMat[0]
+	for i in range(1,m):
+		upperBound = intsMat[0,i]
+		doAssign = assignBits & 1
+		if doAssign==0:
+			retArray[(dataMatrix[:,dimen] < upperBound) & (dataMatrix[:,dimen] >=lowerBound)] = -1.0
+		lowerBound = upperBound
+		assignBits = assignBits >>1
+	doAssign = assignBits & 1
+	if doAssign==0:
+		retArray[dataMatrix[:,dimen] >=lowerBound] = -1.0
 	return retArray
 
-def buildStump(dataArr, classLabels, D):
+def buildSplit(dataArr, classLabels, D):
 	dataMatrix = mat(dataArr);
 	labelMat = mat(classLabels).T 
-	nLabels = labelMat.max()
+	nLabels = int(labelMat.max())
 	m,n = shape(dataMatrix)
-	numSteps = 10.0
-	bestStump = {}
-	bestClasEst = mat(zeros((m,1)))
+	numSteps = 7.0
+	permutations = 2**int(numSteps)
+
+	# could also be one less, to avoid ALL bits to be one.
+	# in the loop later I will start at 1, avoiding ALL bits to be zero
+	# as this would favour a configuration with all parts having NOT class 1 (or other rare class)
+
+	bestSplit = {}
+	bestClassEst = mat(zeros((m,1)))
+	bestClass = 0
 	minError = inf 
 	for i in range(n):
+		print 'Considering feature: %d' % i
 		rangeMin = dataMatrix[:,i].min()
-		rangeMAx = dataMatrix[:,i].max()
-		stepSize = (rangeMAx - rangeMin)/numSteps
-		for j in range(-1, int(numSteps)+1):
-			for assignClass in range(nLabels):
-				lowerLimit = (rangeMin + float(j) * stepSize)
-				if j==0:
-					lowerLimit = 0
-				upperLimit = lowerLimit + stepSize
-				if j==nLabels-1:
-					upperLimit = 10000
-				
-
-				predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
-				errArr = mat(ones((m,1)))
-				errArr[predictedVals == labelMat] = 0
+		rangeMax = dataMatrix[:,i].max()
+		stepSize = (rangeMax - rangeMin)/numSteps
+		intervals = [round(float(x)*stepSize + rangeMin,1) for x in range(int(numSteps))]
+		intervals[0] -= 0.1;
+		# print intervals, rangeMin, rangeMax
+		for c in range(nLabels):
+			assignClass = c+1
+			for assignBits in range(1, permutations):
+				predictedVals = assignIntervals(dataMatrix, i, intervals, assignBits)
+				errArr = mat(zeros((m,1)))
+				errArr[(predictedVals == 1) & (labelMat != assignClass)] = 1
+				errArr[(predictedVals == -1) & (labelMat == assignClass)] =1
 				weightedError = D.T * errArr
-				# print "split: dim %d, thresh %.2f, thresh inequal: \
-				# 	%s, the weighted error is %.3f" %\
-				# 	(i, threshVal, inequal, weightedError)
+				# print "split: dim %d, class %d, split %s, the weighted error is %.3f" %\
+				# 	(i, assignClass, bin(assignBits), weightedError)
 				if weightedError < minError:
 					minError = weightedError
-					bestClasEst = predictedVals.copy()
-					bestStump['dim'] = i 
-					bestStump['thresh'] = threshVal
-					bestStump['ineq'] = inequal
-	return bestStump, minError, bestClasEst
+					bestClassEst = predictedVals.copy()
+					bestClass = assignClass
+					bestSplit['dim'] = i 
+					bestSplit['intervals'] = intervals
+					bestSplit['class'] = assignClass
+					bestSplit['bits'] = assignBits
+	print "best split: dim %d, class %d, split %s, the weighted error is %.3f" %\
+		(bestSplit['dim'], bestSplit['class'], bin(bestSplit['bits']), minError)				
+	return bestSplit, minError, bestClassEst, bestClass
 
-def adaBoostTrainDS(dataArr, classLabels, numIt = 40):
-	weakClassArr = []
-	m = shape(dataArr)[0]
-	D = mat(ones((m,1))/m)
-	aggClasEst = mat(zeros((m,1)))
-	for i in range(numIt):
-		bestStump, error, classEst = buildStump(dataArr, classLabels, D)
-		# print "D:", D.T
-		alpha = float(0.5*log((1.0-error)/max(error, 1e-16)))
-		bestStump['alpha'] = alpha
-		weakClassArr.append(bestStump)
-		# print "classEst: ", classEst.T
-		expon = multiply(-1*alpha*mat(classLabels).T, classEst)
-		D = multiply(D, exp(expon))
-		D = D/D.sum()
-		aggClasEst += alpha*classEst
-		# aggClasEst = (i*aggClasEst + classEst)/(i+1)
-		# print "aggClasEst: ", aggClasEst.T
-		aggErrors = multiply(sign(aggClasEst) != mat(classLabels).T, ones((m,1)) )
-		errorRate = aggErrors.sum()/m
-		print "%d / total error: %.3f" % (i+1, errorRate)
-		if errorRate==0.0: break
-		# raw_input()
-	return weakClassArr, aggClasEst
 
-def adaClassify(datToClass, classifierArr):
-	dataMatrix = mat(datToClass)
-	m = shape(dataMatrix)[0]
-	aggClassEst = mat(zeros((m,1)))
-	for i in range(len(classifierArr)):
-		classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'], \
-			classifierArr[i]['thresh'], \
-			classifierArr[i]['ineq'])
-		aggClassEst += classifierArr[i]['alpha']*classEst
-		print aggClassEst
-	return sign(aggClassEst)
-
-def plotROC(predStrengts, classLabels):
-	import matplotlib.pyplot as plt 
-	cur = (1.0, 1.0)
-	ySum = 0.0
-	numPosClas = sum(array(classLabels)==1.0)
-	yStep = 1/float(numPosClas)
-	xStep = 1/float(len(classLabels)-numPosClas)
-	sortedIndices = predStrengts.argsort()
-	fig = plt.figure()
-	fig.clf()
-	ax = plt.subplot(111)
-	for index in sortedIndices.tolist()[0]:
-		if classLabels[index] == 1.0:
-			delX = 0
-			delY = yStep
-		else:
-			delX = xStep
-			delY = 0
-			ySum += cur[1]
-		ax.plot([cur[0], cur[0]-delX], [cur[1], cur[1]-delY], c='b')
-		cur = (cur[0]-delX, cur[1]-delY)	
-	ax.plot([0,1],[0,1], 'b--')
-	plt.xlabel('False Positive Rate')
-	plt.ylabel('True Positive Rate')
-	plt.title('ROC curve for AdaBoost Horse Colic Detection System')
-	ax.axis([0,1,0,1])
-	plt.show()
-	print "The area under the curve is: ", ySum*xStep
-
-def loadKaggleSet(filename, getClass):
+def loadKaggleSet(filename):
 	numFeat = len(open(filename).readline().split(','))
 	dataMat = []
 	labelMat =[]
